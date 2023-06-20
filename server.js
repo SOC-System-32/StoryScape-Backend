0dfa58562c25cdecca64eb37d5522aa57e3b15cf
Qas1m0m0 <Qasim.17@hotmail.com>
2023-06-20 11:00:20 +0100

    comment out some functions

--- server.js
+++ server.js
@@ -18,106 +18,106 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-function App() {
+// function App() {
 
-  const [userId, setUserId] = useState('');
-  const [media, setMedia] = useState([]);
+//   const [userId, setUserId] = useState('');
+//   const [media, setMedia] = useState([]);
 
-  const getUser = async () => {
+//   const getUser = async () => {
 
-    try {
-      const { data: { user } } = await supabase.auth.getUser()
-      if (user !== null) {
-        setUserId(user.id);
-      } else {
-        setUserId('');
-      }
-    } catch (e) {
-    }
-
-
-
-  }
-
-
-  async function uploadImage(e) {
-    let file = e.target.files[0];
-
-
-    const { data, error } = await supabase
-      .storage
-      .from('media')
-      .upload(userId + "/" + uuidv4(), file)
-
-    if (data) {
-      getMedia();
-
-    } else {
-      console.log(error);
-    }
-  }
-
-  async function getMedia() {
-
-    const { data, error } = await supabase.storage.from('uploads').list(userId + '/', {
-      limit: 10,
-      offset: 0,
-      sortBy: {
-        column: 'name', order:
-          'asc'
-      }
-    });
-
-    if (data) {
-      setMedia(data);
-    } else {
-      console.log(71, error);
-    }
-  }
-
-
-  const signout = async () => {
-    setUserId('');
-    await supabase.auth.signOut();
-  }
+//     try {
+//       const { data: { user } } = await supabase.auth.getUser()
+//       if (user !== null) {
+//         setUserId(user.id);
+//       } else {
+//         setUserId('');
+//       }
+//     } catch (e) {
+//     }
 
 
 
+//   }
 
-  useEffect(() => {
-    getUser();
-    getMedia();
-  }, [userId])
 
-  return (
-    <div className='mt-5'>
-      {userId == '' ? <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-      /> : <>
+//   async function uploadImage(e) {
+//     let file = e.target.files[0];
 
-        <input type="file" onChange={(e) => uploadImage(e)} />
-        <div className='mt-5'>
-          My Uploads
-        </div>
 
-        {media.map((media) => {
-          return (<>
-            <div>
-              <img src={`https://ovlhvogwndcqxaskukrv.supabase.co/storage/v1/object/public/uploads/8cde5d0d-7beb-4ab0-99aa-eaf24e8d2557/${media.name}`} />
-            </div>
-          </>
-          )
-        })}
-        <div className='mt-5'>
-          <button onClick={signout}>
-            Logout
-          </button>
-        </div>
-      </>}
-    </div >
-  )
-}
+//     const { data, error } = await supabase
+//       .storage
+//       .from('media')
+//       .upload(userId + "/" + uuidv4(), file)
+
+//     if (data) {
+//       getMedia();
+
+//     } else {
+//       console.log(error);
+//     }
+//   }
+
+//   async function getMedia() {
+
+//     const { data, error } = await supabase.storage.from('uploads').list(userId + '/', {
+//       limit: 10,
+//       offset: 0,
+//       sortBy: {
+//         column: 'name', order:
+//           'asc'
+//       }
+//     });
+
+//     if (data) {
+//       setMedia(data);
+//     } else {
+//       console.log(71, error);
+//     }
+//   }
+
+
+//   const signout = async () => {
+//     setUserId('');
+//     await supabase.auth.signOut();
+//   }
+
+
+
+
+//   useEffect(() => {
+//     getUser();
+//     getMedia();
+//   }, [userId])
+
+//   return (
+//     <div className='mt-5'>
+//       {userId == '' ? <Auth
+//         supabaseClient={supabase}
+//         appearance={{ theme: ThemeSupa }}
+//       /> : <>
+
+//         <input type="file" onChange={(e) => uploadImage(e)} />
+//         <div className='mt-5'>
+//           My Uploads
+//         </div>
+
+//         {media.map((media) => {
+//           return (<>
+//             <div>
+//               <img src={`https://ovlhvogwndcqxaskukrv.supabase.co/storage/v1/object/public/uploads/8cde5d0d-7beb-4ab0-99aa-eaf24e8d2557/${media.name}`} />
+//             </div>
+//           </>
+//           )
+//         })}
+//         <div className='mt-5'>
+//           <button onClick={signout}>
+//             Logout
+//           </button>
+//         </div>
+//       </>}
+//     </div >
+//   )
+// }
 
 
 app.get("/test", async (req, res) => {
@@ -130,4 +130,4 @@ app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-export default App;
+// export default App;
